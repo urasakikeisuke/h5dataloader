67c4f0d2c59a0c94f77c26235d8fc851582688c2
urasakikeisuke <79469739+urasakikeisuke@users.noreply.github.com>
2022-03-01 09:01:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license='',
     packages=find_packages(),
     install_requires=[
-        "scikit-build", "cmake", "ninja", "numpy", "h5py==2.10.0", "opencv-python-headless", "pointsmap"
+        "scikit-build", "cmake", "ninja", "numpy", "h5py==2.10.0", "opencv-python-headless"
     ],
     python_requires='>=3.6'
 )
